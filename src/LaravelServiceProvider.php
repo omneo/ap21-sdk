070372d30812aaf9b073c9539ba1877d0692fcfa
Andy Johnston <andy@arkade.com.au>
2018-05-09 15:18:52 +1000

    remove http recorder

--- src/LaravelServiceProvider.php
+++ src/LaravelServiceProvider.php
@@ -26,33 +26,9 @@ class LaravelServiceProvider extends ServiceProvider
             $client->setLogging(config('services.apparel21.logging'));
             $client->setVerifyPeer(config('services.apparel21.verify'));
             $client->setLogger(Log::getMonolog());
-
-            $this->setupRecorder($client);
+            $client->setupClient();
 
             return $client;
         });
-    }
-
-    /**
-     * Setup recorder middleware if the HttpRecorder package is bound.
-     *
-     * @param  Client $client
-     * @return Client
-     */
-    protected function setupRecorder(Client $client)
-    {
-        if (! $this->app->bound('Omneo\Plugins\HttpRecorder\Recorder')) {
-            return $client->setupClient();
-        }
-
-        $stack = GuzzleHttp\HandlerStack::create();
-
-        $stack->push(
-            $this->app
-                ->make('Omneo\Plugins\HttpRecorder\GuzzleIntegration')
-                ->getMiddleware(['apparel21', 'outgoing'])
-        );
-
-        return $client->setupClient($stack);
     }
 }
